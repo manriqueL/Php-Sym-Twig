be7a0c24725aa18e3c6f32e923f92596961fa253
manriqueL <manriquelucasp@gmail.com>
2023-08-24 13:13:31 -0300

    tabla de multiplicar

--- src/Controller/GuxController.php
+++ src/Controller/GuxController.php
@@ -8,16 +8,39 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class GuxController extends AbstractController
 {
-    public function index(): Response
+    public function index($nombre, $numero): Response
     {
+        $nombres = array('lucas', 'yesica', 'santiago', 'exequiel', 'carlinho');
+        $apellidos = array( 'manrique', 'acuña', 'fernandez', 'velazques');
+        $titulo = 'TITULO DE INICIO';
         return $this->render('gux/index.html.twig', [
-            'titulo' => 'Lucas Manrique',
+            'titulo' => $titulo,
+            'nombre' => $nombre,
+            'nombres' => $nombres,
+            'apellidos' => $apellidos,
+            'numero' => $numero
         ]);
     }
-    public function team(): Response
+
+    public function team($nombre, $apellido): Response
     {
+        $titulo = 'Titulo de Teams';
         return $this->render('gux/team.html.twig', [
-            'titulo' => '123123123',
+            'titulo' => $titulo,
+            'nombre' => $nombre,
+            'apellido' => $apellido,
         ]);
     }
+    public function table($numero): Response {
+        return $this->render('gux/table.html.twig',[
+            'numero' => $numero
+        ]);
+    }
+    public function redirigir(): Response
+    {
+        // return $this->redirectToRoute('inicio');
+        return $this->redirect('https://www.youtube.com/');
+    }
+
+
 }
